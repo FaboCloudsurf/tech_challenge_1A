090e2f0068d9b73f92c887509ea06bbc3e35d5d9
FaboCloudsurf <FaboCloudsurf@gmail.com>
2026-09-04 21:55:38 -0400

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import './App.css';
-import API_URL from './config'
+// import React, { useEffect, useState } from 'react'
+// import './App.css';
+// import API_URL from './config'
+
+// function App() {
+//   const [successMessage, setSuccessMessage] = useState() 
+//   const [failureMessage, setFailureMessage] = useState() 
+
+//   useEffect(() => {
+//     const getId = async () => {
+//       try {
+//         const resp = await fetch(API_URL)
+//         setSuccessMessage((await resp.json()).id)
+//       }
+//       catch(e) {
+//         setFailureMessage(e.message)
+//       }
+//     }
+//     getId()
+//   })
+
+//   return (
+//     <div className="App">
+//       {!failureMessage && !successMessage ? 'Fetching...' : null}
+//       {failureMessage ? failureMessage : null}
+//       {successMessage ? successMessage : null}
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
-  const [successMessage, setSuccessMessage] = useState() 
-  const [failureMessage, setFailureMessage] = useState() 
+  const [guid, setGuid] = useState('');
 
   useEffect(() => {
-    const getId = async () => {
-      try {
-        const resp = await fetch(API_URL)
-        setSuccessMessage((await resp.json()).id)
-      }
-      catch(e) {
-        setFailureMessage(e.message)
-      }
-    }
-    getId()
-  })
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setGuid(data.guid));
+  }, []);
 
   return (
-    <div className="App">
-      {!failureMessage && !successMessage ? 'Fetching...' : null}
-      {failureMessage ? failureMessage : null}
-      {successMessage ? successMessage : null}
+    <div>
+      <p>{guid}</p>
+
+      <iframe
+        src="https://www.instagram.com/reel/C-8xr7Pq8gj/embed"
+        width="400"
+        height="480"
+        frameBorder="0"
+        scrolling="no"
+        allowFullScreen
+        title="Instagram Reel"
+      ></iframe>
     </div>
   );
 }
-
-export default App;
